9ecab34f04129296d3353450153fd92843cf4fb7
fran <fran.fernandez.rumbo@gmail.com>
2020-02-05 11:16:18 +0100

    Añadiendo el back-End al proyecto (back-end)

---
+++ backEnd/src/main/java/com/springboot/proyectofct/app/ProyectoFctApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.proyectofct.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFctApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/springboot/proyectofct/app/ProyectoFctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.proyectofct.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
