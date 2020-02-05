b0ff8d1d63f6312547de0ae461fdcc5a9460e92f
fran <fran.fernandez.rumbo@gmail.com>
2020-02-05 14:12:50 +0100

    Añadiendo entidades (back-end)
    Añadimos la entidad de curso y usuarios

---
+++ backEnd/src/main/java/com/springboot/proyectofct/app/entity/Course.java
@@ -0,0 +1,109 @@
+package com.springboot.proyectofct.app.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "courses")
+public class Course implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_course;
+	
+	@NotEmpty
+	private String name_course;
+	
+	@NotEmpty
+	private String desc_c;
+	
+	@NotEmpty
+	private String duration;
+	
+	@NotEmpty
+	private String topic;
+	
+	@NotEmpty
+	private String level;
+	
+	@NotNull
+	private boolean enabled;
+	
+	@NotEmpty
+	private String route;
+
+	
+	public long getId_course() {
+		return id_course;
+	}
+
+	public void setId_course(long id_course) {
+		this.id_course = id_course;
+	}
+
+	public String getName_course() {
+		return name_course;
+	}
+
+	public void setName_course(String name_course) {
+		this.name_course = name_course;
+	}
+
+	public String getDesc_c() {
+		return desc_c;
+	}
+
+	public void setDesc_c(String desc_c) {
+		this.desc_c = desc_c;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRoute() {
+		return route;
+	}
+
+	public void setRoute(String route) {
+		this.route = route;
+	}
+
+}
---
+++ backEnd/src/main/java/com/springboot/proyectofct/app/entity/User.java
@@ -0,0 +1,157 @@
+package com.springboot.proyectofct.app.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+
+
+@Entity
+public class User implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_user")
+	private long idUser;
+
+	@NotNull
+	@Column(name = "das_id")
+	private String dasId;
+
+	@NotNull
+	@Column(name = "name_user")
+	private String nameUser;
+
+	@NotNull
+	@Column(name = "lastname_1")
+	private String lastname1;
+
+	@Column(name = "lastname_2")
+	private String lastname2;
+
+	@NotNull
+	@Email
+	private String email;
+
+	// Fecha de nacimiento
+	@Column(name = "birth_d")
+	@Temporal(TemporalType.DATE)
+	private Date birthDate;
+
+	// Numero de telefono
+
+	private String phone;
+
+	// Tipo de identificador: DNI, NIE o pasaporte
+
+	private String document;
+
+	// Numero de la seguridad social
+	@Column(name = "num_ss")
+	private String numSS;
+
+	// private Role rol;
+
+	@NotNull
+	@Column(name = "user_state")
+	private String state;
+
+	public long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getDasId() {
+		return dasId;
+	}
+
+	public void setDasId(String dasId) {
+		this.dasId = dasId;
+	}
+
+	public String getNameUser() {
+		return nameUser;
+	}
+
+	public void setNameUser(String nameUser) {
+		this.nameUser = nameUser;
+	}
+
+	public String getLastname1() {
+		return lastname1;
+	}
+
+	public void setLastname1(String lastname1) {
+		this.lastname1 = lastname1;
+	}
+
+	public String getLastname2() {
+		return lastname2;
+	}
+
+	public void setLastname2(String lastname2) {
+		this.lastname2 = lastname2;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getDocument() {
+		return document;
+	}
+
+	public void setDocument(String document) {
+		this.document = document;
+	}
+
+	public String getNumSS() {
+		return numSS;
+	}
+
+	public void setNumSS(String numSS) {
+		this.numSS = numSS;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+}
